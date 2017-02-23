80fe046503a1a6f4803148409f5760ab7ab50947
Edgardo <emoron@gmail.com>
2017-02-22 21:35:56 -0600

    Update resample2.R

--- Proyecto Tesis Gabriela/resample2.R
+++ Proyecto Tesis Gabriela/resample2.R
@@ -1,10 +1,5 @@
 # Modificacion para Cursos del FCFM
 
-# first try Windows CP1252, although that's almost surely not supported on Mac:
-#Sys.setlocale("LC_ALL", "pt_PT.1252") # Make sure not to omit the `"LC_ALL",` first argument, it will fail.
-#Sys.setlocale("LC_ALL", "pt_PT.CP1252") # the name might need to be 'CP1252'
-
-# next try IS08859-1(/'latin1'), this works for me:
 Sys.setlocale("LC_ALL", "UTF-8")
 library(data.table)
 #library(reshape)
@@ -43,8 +38,8 @@ tableInvert <- function(x){
   dias <- c("LUNES","MARTES","MIERCOLES","JUEVES","VIERNES")
   for ( i in seq(1:5)){
     valor <- as.data.frame(x[i])
-    colnames(valor)[1] <-"HORA"
-    z <- melt(valor,"HORA")
+    colnames(valor)[1] <-"hora"
+    z <- melt(valor,"hora")
     z <- subset(z, value != "")
     z$dia = dias[i]
     salida <- rbind(salida,z)
@@ -57,20 +52,11 @@ tableInvert <- function(x){
 google_docs="https://docs.google.com/spreadsheets/d/1QYHDeZoWcMZjZJo0f0f4RV-UThQS5ahk2sqdh9iqFPc/pub?gid=";
 
 formato = "&single=true&output=csv"
-
-#cars_csv = getURL(paste(google_docs, cars_key, sep = ""))
+#Son los GIDs de las hojas del libro que estamos importando
 
 gids = c("1271806036","1097022579","1442796562","609821330","1342824950")
 
-
-#hojas1 = lapply(edificioo, function(x) { return (paste(google_docs,as.character(x),formato,sep="") )})
-#hojas2 = lapply(edificioi, function(x) { return (paste(google_docs,as.character(x),formato,sep="") )})
-#hojas3 = lapply(edificion, function(x) { return (paste(google_docs,as.character(x),formato,sep="") )})
-#hojas4 = lapply(edificiok, function(x) { return (paste(google_docs,as.character(x),formato,sep="") )})
-
 hojas1 = lapply(gids, function(x) { return (paste(google_docs,as.character(x),formato,sep="") )})
-
-
 
 archivos1 = lapply(hojas1,getURLContent)
 
@@ -79,35 +65,9 @@ tablas1 <- lapply(archivos1,exportTables)
 
 ## Convierto con reshape
 
-salida1 <- tableInvert(tablas1)
-
-
-##
-salida <- data.frame()
-
-salida <- rbind(salida,salida1)
-#salida <- rbind(salida,salida2)
-#salida <- rbind(salida,salida3)
-#salida <- rbind(salida,salida4)
+salida <- tableInvert(tablas1)
 
 salida<- rename(salida, c("variable"="salon", "value" = "materia"))
-salidadt = data.table(salida)
-dt2 <-salidadt
-#dt2$duracion = 3
-#horas = c("7","8","9","10","11","12","13","14","15","16","17","18","19","20")
-
-#salones = c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","")
-#for(i in 1:16){
-#    dt2[salon == paste("X",as.character(i),sep=""), salon := as.character(i)]
-    #dt[horario == paste("H",as.character(i),sep=""),horario := as.numeric(horas[i])]
-#}
-
-
-salida2<- dt2
-
-salida3 <- as.data.frame.matrix(salida2)
-json <- toJSONarray(salida3)
-
-#write(json,file="lista.json")
-
 write.csv(salida,file="~/horarios.csv",row.names = FALSE)
+
+
